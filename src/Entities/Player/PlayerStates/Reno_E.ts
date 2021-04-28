2f1cc66d636823b5dc94c22232437159fc97c099
FranciscoHu17 <francisco.hu17@gmail.com>
2021-04-28 02:17:26 -0400

    Changed reno e

--- src/Entities/Player/PlayerStates/Reno_E.ts
+++ src/Entities/Player/PlayerStates/Reno_E.ts
@@ -1,9 +1,11 @@
 import { Game_Events } from "../../../Enums/GameEvents";
+import BattleManager from "../../../GameSystems/BattleManager";
 import GameLevel from "../../../Scenes/Levels/GameLevel";
 import Vec2 from "../../../Wolfie2D/DataTypes/Vec2";
 import GameEvent from "../../../Wolfie2D/Events/GameEvent";
 import Input from "../../../Wolfie2D/Input/Input";
 import AnimatedSprite from "../../../Wolfie2D/Nodes/Sprites/AnimatedSprite";
+import Sprite from "../../../Wolfie2D/Nodes/Sprites/Sprite";
 import OrthogonalTilemap from "../../../Wolfie2D/Nodes/Tilemaps/OrthogonalTilemap";
 //import { HW4_Events } from "../../hw4_enums";
 import { PlayerStates } from "../PlayerController";
@@ -21,18 +23,42 @@ export default class Reno_E extends PlayerState {
     }
 
 	onEnter(options: Record<string, any>): void {
-		let walls = <OrthogonalTilemap>this.owner.getScene().getLayer("bottom").getItems()[0];
-        var direction = this.owner.direction
-        if(direction.x!= 0){
-            var teleportx = this.owner.position.x + ((128+50) * direction.x);
-            var top = walls.getColRowAt(new Vec2(teleportx, this.owner.boundary.top+30));
-            var middle = walls.getColRowAt(new Vec2(teleportx, this.owner.position.y));
-            var bottom = walls.getColRowAt(new Vec2(teleportx, this.owner.boundary.bottom-30));
-            if((!walls.isTileCollidable(top.x, top.y)) && (!walls.isTileCollidable(bottom.x, bottom.y)) 
-                && (!walls.isTileCollidable(middle.x, middle.y))){
-                this.owner.position.copy(new Vec2(teleportx, this.owner.position.y));
+        let limit = 128*7
+		let enemies = BattleManager.getInstance().getEnemies()
+        
+
+        if (enemies.length > 0){
+            let dist = enemies[0].owner.position.distanceTo(this.owner.position) < limit ? enemies[0].owner.position.distanceTo(this.owner.position) : limit
+            let closest = dist < limit ? enemies[0] : null
+            
+            for(let i = 1; i < enemies.length; i++){
+                let curr_dist = enemies[i].owner.position.distanceTo(this.owner.position)
+                if (curr_dist < dist){
+                    closest = enemies[i]
+                    dist = curr_dist
+                }
+            }
+
+            if(closest != null){
+                let walls = <OrthogonalTilemap>this.owner.getScene().getLayer("bottom").getItems()[0];
+                let teleport = closest.owner.position.clone()
+                teleport.x += 128*3/4 * (<Sprite>closest.owner).direction.x
+                let behind = walls.getTileAtWorldPosition(teleport)
+
+                if(behind == 0){
+                    this.owner.position = teleport;
+                }
+                else{
+                    console.log("cant teleport because of the walls behind the enemy!")
+                }
+
+            }
+            else{
+                console.log("no enemies found")
             }
         }
+        
+
 		this.owner.animation.play("Ability 2 Finish", false);
 		this.retObj = {};
 	}
